1c410f44a03fc501ba442607323ab77560001490
striu21 <134447678+striu21@users.noreply.github.com>
2023-09-14 18:24:44 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,9 @@
-import sus
+import fall
 import platform
 
 if platform.system() == "Windows":
-    if not sus.is_installed("gdown"):
-        sus.run_pip("install gdown", "requirements for Batchlinks Download extension")
+    if not fall.is_installed("gdown"):
+        fall.run_pip("install gdown", "requirements for Batchlinks Download extension")
 
-    if not sus.is_installed("wget"):
-        sus.run_pip("install wget", "requirements for Batchlinks Download extension")
+    if not fall.is_installed("wget"):
+        fall.run_pip("install wget", "requirements for Batchlinks Download extension")
