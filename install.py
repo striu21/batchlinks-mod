2ae2bfacd911342d6b277524f2ed49d9d1130d61
striu21 <134447678+striu21@users.noreply.github.com>
2023-09-14 18:07:52 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,9 +1,9 @@
-import launch
+import sus
 import platform
 
 if platform.system() == "Windows":
-    if not launch.is_installed("gdown"):
-        launch.run_pip("install gdown", "requirements for Batchlinks Download extension")
+    if not sus.is_installed("gdown"):
+        sus.run_pip("install gdown", "requirements for Batchlinks Download extension")
 
-    if not launch.is_installed("wget"):
-        launch.run_pip("install wget", "requirements for Batchlinks Download extension")
+    if not sus.is_installed("wget"):
+        sus.run_pip("install wget", "requirements for Batchlinks Download extension")
